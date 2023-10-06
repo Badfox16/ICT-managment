54aac6125cef3be0eeb6fdd7cdb2fab4db21a5de
Jeremias Adriano <109630558+jeremiasadriano@users.noreply.github.com>
2023-10-06 16:36:30 +0200

    Removação das pastas desnecessárias e adição do dashboard no ICT
    Removi as pastas de conexão, gerente, todos os equipamentos e adicionei o dashboard no ICT Patrimonio

--- Patrimonio/index.php
+++ Patrimonio/index.php
@@ -1,38 +1,221 @@
+<?php
+
+require_once 'Classes/Database/ConexaoBD.php';
+require_once 'Classes/Patrimonio/PatrimonioDAO.php';
+require_once 'Classes/Patrimonio/PatrimonioDTO.php';
+
+$conexao = ConexaoBD::conectar();
+$PatrimonioDAO = new PatrimonioDAO($conexao);
+
+$Patrimonios = $PatrimonioDAO->listarTodos();
+
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <title>Ver Patrimonios</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" />
+  <link rel="stylesheet" href="./style/bootstrap.min.css" crossorigin="anonymous">
+  <link rel="stylesheet" href="./style/style.css" />
 </head>
+
 <body>
+  <main class="main container-fluid d-flex">
+    <!-- Dashboard/ left content -->
+    <section class="left-dashboard col-2">
+      <div class="fixed-top">
+        <div class="d-flex flex-column p-3 text-white bg-dark" style="width: 300px; height:100vh;">
+          <a href="/" class="d-flex flex-column align-items-center mb-3 text-white text-decoration-none">
+            <span class="fs-4">Equipamentos</span>
+            <span class="fs-4">ICT</span>
+          </a>
+          <hr>
+          <ul class="nav nav-pills flex-column mb-auto">
+            <li class="nav-item">
+              <a href="#" class="nav-link active mt-4 mb-4" aria-current="page">
+                <svg class="bi me-2" width="16" height="16">
+                  <use xlink:href="#home"></use>
+                </svg>
+                Gerentes
+              </a>
+            </li>
+            <li>
+              <a href="#" class="nav-link text-white mb-4">
+                <svg class="bi me-2" width="16" height="16">
+                  <use xlink:href="#speedometer2"></use>
+                </svg>
+                Equipamentos
+              </a>
+            </li>
+            <li>
+              <a href="#" class="nav-link text-white mb-4">
+                <svg class="bi me-2" width="16" height="16">
+                  <use xlink:href="#table"></use>
+                </svg>
+                Relatórios
+              </a>
+            </li>
+            <li>
+              <a href="#" class="nav-link text-white mb-4">
+                <svg class="bi me-2" width="16" height="16">
+                  <use xlink:href="#grid"></use>
+                </svg>
+                Registros
+              </a>
+            </li>
+            <li>
+              <a href="#" class="nav-link text-white">
 
-    <h1>Cadastrar Gerente do Patrimonio</h1>
+            </li>
+          </ul>
+          <hr>
+          <div class="dropdown">
+            <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+              <strong>Logout</strong>
+            </a>
+            <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
+              <li><a class="dropdown-item" href="#">Definições</a></li>
+              <li><a class="dropdown-item" href="#">Ver Perfil</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="#">Sair</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
 
-    <form action="Controllers/Cadastrar.php" method="POST">
+    <!-- Right content -->
+    <section class="right-content container-fluid bg-muted pt-4">
+      <div class="header d-flex justify-content-between text-muted">
+        <div class="system-name">
+          <h3 class="text-uppercase mx-4 text-dark">Ict-patrimorio</h3>
+        </div>
 
-    <input type="text" name="nome" placeholder="nome" required>
-    <br><br>
-    <input type="text" name="apelido" placeholder="apelido" required>
-    <br><br>
-    <input type="text" name="contacto" placeholder="contacto" required>
-    <br><br>
-    <input type="text" name="login" placeholder="login" required>
-    <br><br>
-    <input type="text" name="estado" value="Ativo" hidden required>
-    <br><br>
-    <input type="text" name="senha" placeholder="senha" required>
-    <br><br>
-    <input type="submit" value="Cadastrar">
-    </form>
-    
-    <br>
+        <div class="manager-name">
+          <h5 class="text-black">Seja bem vindo de volta</h5>
+          <p class="text-end text-dark">Felizardo Carlos</p>
+        </div>
+      </div>
 
-    <br>
-    <h2>Clique aqui para ver todos os gerentes do Patrimonio</h2>
-    <form method="post" action="Controllers/outra_pagina.php">
-    <input type="submit" name="redirecionar" value="Redirecionar para Outra Página">
-</form>
+      <div class="main-content">
+        <div class="reporters container p-3">
+          <h2 class="text-primary">Reporter</h2>
 
+          <div class="reporters-form">
+            <form action="">
+              <table class="table table-borderless">
+                <thead>
+                  <tr>
+                    <th>Categoria Equipamento</th>
+                    <th>Data Inicial</th>
+                    <th>Data Final</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td class="col-1">
+                      <select class="form-select">
+                        <option value="1">Hardware</option>
+                        <option value="2">Software</option>
+                        <option value="3" selected>Todos</option>
+                      </select>
+                    </td>
+                    <td class="col-1">
+                      <input type="date" class="form-control" />
+                    </td>
+                    <td class="col-sm-1">
+                      <input type="date" class="form-control" />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="d-flex justify-content-around">
+                      <button class="search-btn bg-primary">
+                        <i class="bi bi-search"></i>
+                        Pesquisar
+                      </button>
+                    </td>
+                    <td></td>
+                    <td>
+                      <button type="reset" class="reset-btn bg-danger">
+                        <i class="bi bi-trash3"></i>
+                        Limpar
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </form>
+          </div>
+          <div class="reports-pdf container p-1">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th>Reportes</th>
+                </tr>
+              </thead>
+              <tbody></tbody>
+            </table>
+          </div>
+        </div>
+        <div class="managers container p-1">
+          <div class="managers-title-add">
+            <div class="manager-title text-uppercase">
+              <p>Membros do Património</p>
+            </div>
+            <div class="manager-add">
+              <button class="btn btn-success text-white">
+                <i class="bi bi-person-plus"></i>
+                Adicionar membro
+              </button>
+            </div>
+          </div>
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>Id</th>
+                <th>Email</th>
+                <th>Telefone</th>
+                <th>Estado</th>
+                <th>Perfil</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php foreach ($Patrimonios as $usuarios) : ?>
+                <tr>
+                  <td><?= $usuarios["nome"] ?></td>
+                  <td><?= $usuarios["id"] ?></td>
+                  <td>adrianojeremias1@gmail.com</td>
+                  <td>855271154</td>
+                  <td>
+                    <?php
+                    if ($usuarios["isAdmin"]) {
+                      echo "<button disabled class='manager-actived'>Ativo</button>";
+                    } else {
+                      echo "<button disabled class='manager-disabled'>Inativo</button>";
+                    }
+                    ?>
+                  </td>
+                  <td>
+                    <a href="./UsuarioAtualizar.php?usuarioTK=<?= $usuarios["id"] ?>">
+                      <button class="btn btn-danger">
+                        <i class="bi bi-pencil-square"></i>
+                      </button>
+                    </a>
+                  </td>
+                </tr>
+              <?php endforeach; ?>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </section>
+  </main>
 </body>
+
 </html>
