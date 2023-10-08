f14cff7b8f8147a39d4581db9a0f6016d2baa1e1
Jeremias Adriano <109630558+jeremiasadriano@users.noreply.github.com>
2023-10-08 06:52:09 +0200

    Adição do php no logs

--- Logs/Classes/Logs/LogsDAO.php
+++ Logs/Classes/Logs/LogsDAO.php
@@ -3,11 +3,18 @@ require_once "../Database/ConexaoBD.php";
 require_once "../Logs/LogsDTO.php";
 class LogsDAO
 {
+    private $conexao;
+
+    public function __construct($conexao)
+    {
+        $this->conexao = $conexao;
+    }
+
     public function listarTodos()
     {
-        $sql = "SELECT * FROM logs";
-        $resultado = ConexaoBD::conectar()->query($sql);
-        $logs = $resultado->fetchAll(PDO::FETCH_ASSOC);
-        return $logs;
+        $sql = "SELECT * FROM tbPatrimonio";
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
