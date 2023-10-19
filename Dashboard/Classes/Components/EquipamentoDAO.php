af78d83d8417598a7a5664d89bfd017ea46dc1e5
Jeremias Adriano <109630558+jeremiasadriano@users.noreply.github.com>
2023-10-19 19:52:07 +0200

    Adicao da pasta components. Ela contém todos os métodos DAO e DTO necessários para listar os equipamentos

---
+++ Dashboard/Classes/Components/EquipamentoDAO.php
@@ -0,0 +1,110 @@
+<?php
+
+require_once 'EquipamentoDTO.php'; // Inclua o arquivo com a definição da classe EquipamentoDTO
+require_once __DIR__ . '/../Database/ConexaoBD.php';
+
+class EquipamentoDAO
+{
+    private $conexao;
+
+    public function __construct($conexao)
+    {
+        $this->conexao = $conexao;
+    }
+
+    public function inserir(EquipamentoDTO $equipamento)
+    {
+        $sql = "INSERT INTO tbEquipamento (Tipo, Marca, Modelo, NrDeSerie, Estado, Localizacao, Fornecedor, DataFornecimento, DescricaoEquipamento, Observacoes) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->execute([$equipamento->getTipo(), $equipamento->getMarca(), $equipamento->getModelo(), $equipamento->getNrDeSerie(), $equipamento->getEstado(), $equipamento->getLocalizacao(), $equipamento->getFornecedor(), $equipamento->getDataFornecimento(), $equipamento->getDescricaoEquipamento(), $equipamento->getObservacoes()]);
+    }
+
+    public function atualizar(EquipamentoDTO $equipamento)
+    {
+        $sql = "UPDATE tbEquipamento SET Tipo = ?, Marca = ?, Modelo = ?, NrDeSerie = ?, Estado = ?, Localizacao = ?, Fornecedor = ?, DataFornecimento = ?, DescricaoEquipamento = ?, Observacoes = ? WHERE Id_Equipamento = ?";
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->execute([$equipamento->getTipo(), $equipamento->getMarca(), $equipamento->getModelo(), $equipamento->getNrDeSerie(), $equipamento->getEstado(), $equipamento->getLocalizacao(), $equipamento->getFornecedor(), $equipamento->getDataFornecimento(), $equipamento->getDescricaoEquipamento(), $equipamento->getObservacoes(), $equipamento->getIdEquipamento()]);
+    }
+
+    public function remover($id)
+    {
+        $sql = "DELETE FROM tbEquipamento WHERE Id_Equipamento = ?";
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->execute([$id]);
+    }
+
+    public function buscarPorId($id)
+    {
+        $sql = "SELECT 
+        e.Id_Equipamento, 
+        t.Tipo, 
+        e.Marca, 
+        e.Modelo, 
+        e.NrDeSerie, 
+        e.Estado, 
+        s.NomeSala as Localizacao, 
+        e.Fornecedor, 
+        e.DataFornecimento, 
+        e.DescricaoEquipamento, 
+        e.Observacoes
+    FROM 
+        tbEquipamento e
+    JOIN 
+        tbTipo t ON e.Tipo = t.Id_Tipo
+    JOIN 
+        tbSala s ON e.Localizacao = s.Id_Sala WHERE e.Id_Equipamento = ?";
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->execute([$id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+
+
+    public function listarTodos()
+    {
+        $sql = "SELECT 
+        e.Id_Equipamento, 
+        t.Tipo, 
+        e.Marca, 
+        e.Modelo, 
+        e.NrDeSerie, 
+        e.Estado, 
+        s.NomeSala as Localizacao, 
+        e.Fornecedor, 
+        e.DataFornecimento, 
+        e.DescricaoEquipamento, 
+        e.Observacoes
+    FROM 
+        tbEquipamento e
+    JOIN 
+        tbTipo t ON e.Tipo = t.Id_Tipo
+    JOIN 
+        tbSala s ON e.Localizacao = s.Id_Sala
+    ";
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->execute();
+
+        $equipamentos = [];
+
+        while ($resultado = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            // Criando um novo objeto EquipamentoDTO com os atributos básicos
+            $equipamento = new EquipamentoDTO();
+            $equipamento->setIdEquipamento($resultado['Id_Equipamento']);
+            $equipamento->setTipo($resultado['Tipo']); // Tipo agora é o nome do tipo
+            $equipamento->setMarca($resultado['Marca']);
+            $equipamento->setModelo($resultado['Modelo']);
+            $equipamento->setNrDeSerie($resultado['NrDeSerie']);
+            $equipamento->setEstado($resultado['Estado']);
+            $equipamento->setLocalizacao($resultado['Localizacao']);
+            $equipamento->setFornecedor($resultado['Fornecedor']);
+            $equipamento->setDataFornecimento($resultado['DataFornecimento']);
+            $equipamento->setDescricaoEquipamento($resultado['DescricaoEquipamento']);
+            $equipamento->setObservacoes($resultado['Observacoes']);
+
+            // Adicionando o objeto ao array
+            $equipamentos[] = $equipamento;
+        }
+
+        return $equipamentos;
+    }
+}
---
+++ Dashboard/Classes/Components/TiposDAO.php
@@ -0,0 +1,90 @@
+<?php
+
+require_once 'TiposDTO.php';
+require_once __DIR__ . '/../Database/ConexaoBD.php';
+
+class TiposDAO
+{
+    private $conexao;
+
+    public function __construct($conexao)
+    {
+        $this->conexao = $conexao;
+    }
+
+    public function inserirTipo($tipo)
+    {
+        $sql = "INSERT INTO tbTipo (Tipo) VALUES (:tipo)";
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindParam(':tipo', $tipo, PDO::PARAM_STR);
+        return $stmt->execute();
+    }
+
+    public function listarTodos()
+    {
+        $sql = "SELECT * FROM tbTipo";
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->execute();
+
+        $tipos = [];
+
+        while ($resultado = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $tipo = new TiposDTO();
+            $tipo->setIdTipo($resultado['Id_Tipo']);
+            $tipo->setTipo($resultado['Tipo']);
+            $tipos[] = $tipo;
+        }
+
+        return $tipos;
+    }
+
+    public function deletarTipo($idTipo)
+    {
+        $sql = "DELETE FROM tbTipo WHERE Id_Tipo = :idTipo";
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindParam(':idTipo', $idTipo, PDO::PARAM_INT);
+        return $stmt->execute();
+    }
+
+    public function atualizarTipo($idTipo, $tipo)
+    {
+        $sql = "UPDATE tbTipo SET Tipo = :tipo WHERE Id_Tipo = :idTipo";
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindParam(':tipo', $tipo, PDO::PARAM_STR);
+        $stmt->bindParam(':idTipo', $idTipo, PDO::PARAM_INT);
+        return $stmt->execute();
+    }
+
+    public function existeTipoComNome($nomeTipo)
+    {
+        $sql = "SELECT COUNT(*) FROM tbTipo WHERE Tipo = :nomeTipo";
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindParam(':nomeTipo', $nomeTipo, PDO::PARAM_STR);
+        $stmt->execute();
+        $count = $stmt->fetchColumn();
+
+        return $count > 0;
+    }
+
+    public function existeTipoComNome2($novoTipo)
+    {
+        $sql = "SELECT COUNT(*) FROM tipos WHERE nome = :nome";
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(':nome', $novoTipo);
+        $stmt->execute();
+        $count = $stmt->fetchColumn();
+
+        // Se count for maior que 0, significa que o tipo já existe
+        return $count > 0;
+    }
+
+
+    // Na classe TiposDAO, adicione a função deletarTipoPorNome
+    public function deletarTipoPorNome($nomeTipo)
+    {
+        $sql = "DELETE FROM tbTipo WHERE Tipo = :nomeTipo";
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindParam(':nomeTipo', $nomeTipo, PDO::PARAM_STR);
+        return $stmt->execute();
+    }
+}
