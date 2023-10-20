43ce27baa854aa81ac6ccd054776b86b6e517d7a
badfox16 <kingbadfox3@gmail.com>
2023-10-20 14:38:47 +0200

    Jeremias, script da Dashboard bugou

---
+++ Dashboard/Classes/Components/SoftwareDAO.php
@@ -0,0 +1,29 @@
+<?php
+require_once 'SoftwareDTO.php';
+require_once __DIR__ . '/../Database/ConexaoBD.php';
+
+class SoftwareDAO
+{
+    private $conexao;
+
+    public function __construct($conexao)
+    {
+        $this->conexao = $conexao;
+    }
+
+    public function listarTodos()
+    {
+        $sql = "SELECT * FROM tbSoftware";
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->execute();
+
+        $softwares = [];
+
+        while ($resultado = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $software = new SoftwareDTO($resultado['Id_Software'], $resultado['Id_Equipamento'], $resultado['NomeSoftware'], $resultado['Fabricante'], $resultado['Versao'], $resultado['Estado'], $resultado['Observacoes'], $resultado['DiaInstalacao'], $resultado['DiaExpiracao']);
+            $softwares[] = $software;
+        }
+
+        return $softwares;
+    }
+}
